03a1a205d42c8e9f51cdde537531dd8ad28efeed
kiorky <devnull@localhost>
2008-05-19 11:20:32 +0200

    0.4 changes

--- hooks/proj.py
+++ hooks/proj.py
@@ -3,7 +3,10 @@ import os,sys
 
 def installmaps(options,buildout):
     # adding maps files
-    os.system('cd nad')
-    os.system('tar xzvf %s/maps/proj-datumgrid-1.3.tar.gz'% buildout['buildout']['directory'])
-    os.system('cd ..')
+    import pdb;pdb.set_trace()  ## Breakpoint ##
+    os.system("""
+cd %s/nad
+tar xzvf %s/maps/proj-datumgrid-1.3.tar.gz
+cd .. """ %( options['compile-directory'], buildout['buildout']['directory']))
+
 
